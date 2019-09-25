f2775a863b3994db311798a3decf39d26c3a5c01
Damien Jiale LIEW <damienliewjl@gmail.com>
2019-09-25 15:52:34 +0800

    Init

---
+++ SVY21-to-WGS84.R
@@ -0,0 +1,14 @@
+library(rgdal)
+
+data <- read.csv("C:/Users/damienliew/OneDrive - Far East Management Pte Ltd/Desktop/Time to TOP/Output1.csv")
+x <- data$X.Addr
+y <- data$Y.Addr
+d <- data.frame(lon=x, lat= y)
+coordinates(d) <- c("lon", "lat")
+
+CRS.new <- CRS("+init=epsg:4326") #WGS84
+proj4string(d) <- CRS("+init=epsg:3414") # SVY21
+
+d.wgs84 <- spTransform(d, CRS.new)
+d.wgs84 <- as.data.frame(d.wgs84)
+data <- cbind(d.wgs84, data)
